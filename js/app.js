0cf11fc521dcea30933d4c70ac97e293316397bf
Conny Garpö <dolgan12@gmail.com>
2016-01-09 11:53:49 +0100

    Finnished

--- js/app.js
+++ js/app.js
@@ -5,17 +5,37 @@ $(document).ready(function(){
     .subscribe(function(data){
       var news = JSON.parse(data.response);
       news.forEach(function(item){
-        var link = item.link;
+        var link = encodeURI(item.link);
         var image = item.author.picture;
-        var userPage = userUrl + item.author.username;
+        var userPage = userUrl +'/' + item.author.username;
         var likes = item.upVotes.length;
-        var timePosted = new Date(item.timePosted);
-        var rubrik = (item.headline.length > 20) ? item.headline.substr(0, 20) + "..." : item.headline;
+        var daysSincePost = function (){
+          var days = Math.round(Math.abs((Date.now() - item.timePosted) / (86400 * 1000)));
+          switch (days) {
+            case 0:
+              return "Today";
+            case 1:
+                return "Yesterday";
+            default:
+                return days + " days ago";
+          }
+        };
 
+        var rubrik = (item.headline.length > 20) ? item.headline.substr(0, 20) + "..." : item.headline;
         //  Generate the html
-        $('.container').append();
-        console.log(rubrik);
-        console.log(timePosted.toDateString());
+        var html =
+         '<div class="card">' +
+          '<div class="authImg">' +
+          '<a target="_blank" href="' + link + '"><img src="' + image + '" title="' + item.headline + '"/></a>' +
+          '</div>' +
+          '<div class="textArea">' +
+          '<div class="rubrik"><a href="' + link + '" target="_blank" title="' + item.headline + '">' + rubrik + '</a></div>' +
+          '<div class="author"><a href="' + userPage + '" target="_blank">By: ' + item.author.username + '</a></div>' +
+          '<div class="likes">&#10084; ' + likes + '</div>' +
+          '<div class="datePosted">Posted: ' + daysSincePost() + '</div>' +
+          '</div>' +
+          '</div>';
+        $('.container').append(html);
       });
     });
 });
